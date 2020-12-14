5aeedcb83f5ba0bcd1742f41f420768b02c9e316
TallaVa <behram89@gmail.com>
2020-12-14 15:14:27 +0100

    uppdaterad mapp och nya uppgifter

---
+++ ClassTest.java
@@ -0,0 +1,25 @@
+/**
+ * This program demonstrates the Rectangle class's
+ * setLength method.
+ */
+
+public class ClassTest 
+{
+    public static void main(String [] args)
+    {
+        // Create a rectangle object and assign its
+        // adress to the box variable.
+
+        Rectangle box = new Rectangle();
+
+        // Indicate what we ar doing.
+        System.out.println("Sending the value 10.0 to the setLengh method.");
+
+        // Call the box object's setLength method.
+        box.setLength(10.0);
+
+        // Indicate we are done.
+        System.out.println("Done.");
+    }
+    
+}
