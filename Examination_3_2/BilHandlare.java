1ae532a9105408a692d75e162a2b1737f4f3062c
TallaVa <behram89@gmail.com>
2021-04-01 00:03:59 +0200

    allt klart

--- Examination_3_2/BilHandlare.java
+++ Examination_3_2/BilHandlare.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 
 public class BilHandlare 
 {
-
+   // Skapar Scanner som anänds för att registrera input.
    //Privata variabler som kommer användas i klassen.
     private static Scanner keyboard = new Scanner(System.in);
 
     //Skapa array som ska användas för att hålla alla  bilar.
     // kommer ha plats för 20 bilar.
-    private static Bil bilarArray[] = new Bil[4];
-    private static int index = 0;
+    private static Bil bilarArray[] = new Bil[10];
+    private static int index =0;
+    
     
 
 
@@ -44,7 +45,7 @@ public class BilHandlare
       utskriftMeny();
       while (!exit)
       {
-         System.out.print("Välj i menyn: ");
+         System.out.print("Val: ");
          val = keyboard.nextInt();
          keyboard.nextLine();
 
@@ -54,15 +55,15 @@ public class BilHandlare
                   utskriftMeny();      
                   break;
             case 1:
-                  if(index <4 )
-                  laggtillBil();
+                  if ( index >= 10)
+                  {
+                     System.out.println("Listan är full.");   
+                  } 
                   else
                   {
-                     System.out.println("Listan är full." +
-                        " Ta bort bil för att lägga till ny bil.");
-                  }
-                  break; 
-                    
+                     laggtillBil();
+                  }                
+                  break;                    
             case 2:
                   andraMil(bilarArray);
                   break;         
@@ -74,6 +75,15 @@ public class BilHandlare
                   break;
             case 5:
                   geRabatt(bilarArray);
+                  break;
+            case 6:
+                  sokMarke(bilarArray);
+                  break;
+            case 7:
+                  sokBil(bilarArray);
+                  break;
+            case 8:
+                  kopAllaBilar(bilarArray);
                   break;
             case 9:
                   exit = true;
@@ -97,17 +107,20 @@ public static void utskriftMeny()
    System.out.println("\t 2 - Ändra miltal på bil i listan.");
    System.out.println("\t 3 - Ta bort bil.");
    System.out.println("\t 4 - Skriv ut allt i arrayn.");
-   System.out.println("\t 5 - Ge rabatt.\n");
+   System.out.println("\t 5 - Ge rabatt.");
+   System.out.println("\t 6 - Sök efter bilar baserat på märke.");
+   System.out.println("\t 7 - Sök information på registrerad bil.");
+   System.out.println("\t 8 - Visa pris för alla bilar tillsammans.\n");
 }
 
 /**
- * regExisterar metoden kommer att kontrolera så det inte 
+ * regnrExisterar metoden kommer att kontrolera så det inte 
  * finns dubbletter innan man kan lägga till nya bilar i
  * arrayn.
  * @param listaBilar
  * @return Om det finns dublett eller inte.
  */
-public static boolean regExisterar(Bil[] listaBilar)
+public static boolean regnrExisterar(Bil[] listaBilar)
 {
    boolean existerar = false;
 
@@ -134,12 +147,11 @@ public static boolean regExisterar(Bil[] listaBilar)
 public static void laggtillBil()
 {
    laggTillNyBil(bilarArray, index);
-   if (regExisterar(bilarArray))
+   if (regnrExisterar(bilarArray))
    {
-      System.out.println("En bil existerar redan med samma regnr. Ange en ny.\n");
-      laggTillNyBil(bilarArray, index);
+      System.out.println("En bil existerar redan med samma regnr.\n");
+      index--;
    }
-   index++;
 }
 
 
@@ -183,9 +195,8 @@ public static void laggTillNyBil(Bil[] listaBilar, int i)
    String ovrigt = keyboard.nextLine();
 
    //Dubbelkollar så arrayn inte är "out of bounds"
-   // och då lägger in informationen man angav in i arrayn.
-   
-   if (index <= 4)
+   // och då lägger in informationen man angav in i arrayn.   
+   if (index < 10)
       { 
          for ( int j = 0; j < listaBilar.length; j++)
          {
@@ -195,12 +206,16 @@ public static void laggTillNyBil(Bil[] listaBilar, int i)
                listaBilar[i] = new Bil(regNr, marke, model, Integer.parseInt(artal), Integer.parseInt(miltal), vaxel, farg, drivmedel, Double.parseDouble(pris), ovrigt);
                System.out.println("\tBil registrerat.\n");
                utskriftMeny(); 
+               System.out.println(index);
+               index++;
+               System.out.println(index);
+               break;
             }
          }   
       } 
-   else if (index > 4 )
+   else if (index > 10 )
       {
-         System.out.println("Listan är full");
+         System.out.println("Listan är full\n");
          utskriftMeny();
       }
      
@@ -214,7 +229,7 @@ public static void laggTillNyBil(Bil[] listaBilar, int i)
 public static void andraMil(Bil[] listaBilar)
 {
    System.out.println("Ange regnr för den bil du vill "
-                     +"uppdatera miltalen på: ");
+                     +"uppdatera miltalen på: \n");
       String regNr = keyboard.nextLine();
    System.out.println("Ange det uppdaterade miltalen: ");
       int nyaMil = keyboard.nextInt();
@@ -222,17 +237,22 @@ public static void andraMil(Bil[] listaBilar)
    for (int i = 0; i < listaBilar.length; i++)
    {
       if (listaBilar[i] != null && listaBilar[i].getregNr().equalsIgnoreCase(regNr))
-      
-         listaBilar[i].setmiltal(nyaMil);      
-      else
-         System.out.println("Fel regnr eller så existerar inte bilen.\n");
-         break;
+         {
+            listaBilar[i].setmiltal(nyaMil);
+            System.out.println("---- Uppdaterad ----");
+         } 
    }
+   System.out.println("\n");
+   utskriftMeny();
 }
 
+/**
+ * tabortBil metoden tar bort vald bil genom regnr.
+ * @param listaBilar
+ */
 public static void tabortBil(Bil[] listaBilar)
 {
-   System.out.println("Ange regnr på bil som önskas tas bort från listan: ");
+   System.out.println("Ange regnr på bil som önskas tas bort från listan: \n");
       String regNr = keyboard.nextLine();
 
       for (int i = 0; i < listaBilar.length; i ++)
@@ -241,11 +261,12 @@ public static void tabortBil(Bil[] listaBilar)
          {
             listaBilar[i] = null;
             index--;
+            System.out.println(index);
             break;
          }
          if (i == listaBilar.length -1)
          {
-            System.out.println("Bil med regnr " +regNr + " existerar ej.");
+            System.out.println("Bil med regnr " +regNr + " existerar ej.\n");
          }
       }
 }
@@ -279,7 +300,7 @@ public static void geRabatt(Bil[] listaBilar)
 
       for (int i = 0; i < listaBilar.length; i++)
       {
-         if (listaBilar[i] != null && listaBilar[i].getartal() > input)
+         if (listaBilar[i] != null && listaBilar[i].getartal() < input)
          {
             System.out.println("------------------\n");
             System.out.println(listaBilar[i].getregNr());
@@ -290,13 +311,84 @@ public static void geRabatt(Bil[] listaBilar)
             System.out.println(listaBilar[i].getvaxel());
             System.out.println(listaBilar[i].getfarg());
             System.out.println(listaBilar[i].getdrivmedel());
-            System.out.println(listaBilar[i].getpris() + " kr\t" + listaBilar[i].getRabattpris() + " kr nytt pris");
+            System.out.println(listaBilar[i].getpris() + " kr\t" 
+                                 + listaBilar[i].getRabattpris() 
+                                 + " kr nytt pris");
             System.out.println(listaBilar[i].gerovrigt() + "\n");
-            
             double nyttPris = listaBilar[i].getRabattpris();
             listaBilar[i].setpris(nyttPris);
          }
          
       }
-}
+      System.out.println("\n");
+      utskriftMeny();
+   }
+
+   /**
+    * Den här metoden kommer visa information 
+    * om bilar baserad på Märket på bilar
+    * man sökt efter.
+    * @param listaBilar
+    */
+   public static void sokMarke(Bil[] listaBilar)
+   {
+      System.out.println("Efter vilket märke vill du söka ? ");
+      String marke = keyboard.nextLine();
+
+      for (int i = 0; i < listaBilar.length; i++)
+      {
+         if (listaBilar[i] != null && listaBilar[i].getmarke().equalsIgnoreCase(marke))
+         {
+            System.out.println("------------------\n");
+            System.out.println(listaBilar[i].toString());
+         }
+         
+      }
+      System.out.println("\n");
+      utskriftMeny();
+   }
+
+   /**
+    * sokBil metoden tillåter användaren 
+    * att söka information om enstaka bilar.
+    * @param listaBilar
+    */
+   public static void sokBil(Bil[] listaBilar)
+   {
+      System.out.println("Ange regnr på bilen du söker ? " 
+                           +"\n(tom eller fel reg tar tig tillbaka till menyn.)");
+      String reg = keyboard.nextLine();
+
+      for (int i = 0; i < listaBilar.length; i++)
+      {
+         if (listaBilar[i] != null && listaBilar[i].getregNr().equalsIgnoreCase(reg))
+            {
+               System.out.println("------------------\n");
+               System.out.println(listaBilar[i].toString());    
+               break;           
+            }   
+      }
+      System.out.println("\n");
+      utskriftMeny();
+   }
+
+   /**
+    * Metoden listar total priset för alla bilar 
+    * i listan.
+    * @param listaBilar
+    */
+   public static void kopAllaBilar(Bil[] listaBilar)
+   {
+      double sum = 0;
+
+      for (int i = 0; i < listaBilar.length; i++)
+      {
+         if (listaBilar[i] != null)
+         {
+         sum += listaBilar[i].getpris();
+         }
+      }
+      System.out.printf("%.2f", sum);
+      System.out.println(" kr för alla bilar i listan.\n");
+   }
 }
